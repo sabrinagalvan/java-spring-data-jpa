81ec4bd88d428f3f1b29227126747a850feab33c
sabrinagalvan <sabrinagalvans2@gmail.com>
2023-11-25 00:35:30 -0300

    atualizando continuacao do projeto de estudo

--- spring-data/src/main/java/br/com/alura/spring/data/SpringDataApplication.java
+++ spring-data/src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -5,19 +5,33 @@ import java.util.Scanner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import br.com.alura.spring.data.orm.Cargo;
-import br.com.alura.spring.data.source.CrudCargoService;
+import br.com.alura.spring.data.service.CrudCargoService;
+import br.com.alura.spring.data.service.CrudFuncionarioService;
+import br.com.alura.spring.data.service.CrudUnidadeTrabalhoService;
+import br.com.alura.spring.data.service.RelatoriosService;
 
+@EnableJpaRepositories
 @SpringBootApplication
 public class SpringDataApplication implements CommandLineRunner{
 
 	private final CrudCargoService cargoService;
+	private final CrudFuncionarioService funcionarioService;
+	private final CrudUnidadeTrabalhoService unidadeTrabalhoService;
+	private final RelatoriosService relatoriosService;
 	
 	private Boolean system = true;
 	
-	public SpringDataApplication(CrudCargoService cargoService) {
+	public SpringDataApplication(CrudCargoService cargoService,
+			CrudFuncionarioService funcionarioService,
+			CrudUnidadeTrabalhoService unidadeTrabalhoService,
+			RelatoriosService relatoriosService) {
 		this.cargoService = cargoService;
+		this.funcionarioService = funcionarioService;
+		this.unidadeTrabalhoService = unidadeTrabalhoService;
+		this.relatoriosService = relatoriosService;
+		
 	}
 	
 	public static void main(String[] args) {
@@ -32,13 +46,30 @@ public class SpringDataApplication implements CommandLineRunner{
 			System.out.println("Qual ação você deseja executar?");
 			System.out.println("0 - Sair");
 			System.out.println("1 - Cargo");
+			System.out.println("2 - Funcionário");
+			System.out.println("3 - Unidade de Trabalho");
+			System.out.println("4 - Relatórios");
 			
 			int action = scanner.nextInt();
-			if(action == 1) {
+			
+			switch (action) {
+			case 1:
 				cargoService.inicial(scanner);
-			}else {
+				break;
+			case 2:
+				funcionarioService.inicial(scanner);
+				break;
+			case 3:
+				unidadeTrabalhoService.inicial(scanner);
+				break;
+			case 4:
+				relatoriosService.inicial(scanner);
+				break;
+			default:
 				system = false;
+				break;
 			}
+			
 		}
 		
 	}
