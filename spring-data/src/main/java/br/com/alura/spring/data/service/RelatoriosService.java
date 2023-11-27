d364dadcca1e75f759126a6cee4436c633e7f2c4
sabrinagalvan <sabrinagalvans2@gmail.com>
2023-11-27 20:24:25 -0300

    atualizando continuacao do projeto estudo

--- spring-data/src/main/java/br/com/alura/spring/data/service/RelatoriosService.java
+++ spring-data/src/main/java/br/com/alura/spring/data/service/RelatoriosService.java
@@ -5,11 +5,10 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Scanner;
 
-import javax.swing.text.DateFormatter;
-
 import org.springframework.stereotype.Service;
 
 import br.com.alura.spring.data.orm.Funcionario;
+import br.com.alura.spring.data.orm.FuncionarioProjecao;
 import br.com.alura.spring.data.repository.FuncionarioRepository;
 
 @Service
@@ -31,6 +30,7 @@ public class RelatoriosService {
 			System.out.println("1 - Qual o nome do funcionário?");
 			System.out.println("2 - Qual nome, data da contratação e salário?");
 			System.out.println("3 - Qual a data da contratação?");
+			System.out.println("4 - Pesquisar funcionário pelo salário");
 			
 			int action = scanner.nextInt();
 			
@@ -43,6 +43,9 @@ public class RelatoriosService {
 				break;
 			case 3:
 				buscarFuncionarioDataContratacao(scanner);
+				break;
+			case 4:
+				pesquisaFuncionarioSalario();
 				break;
 			default:
 				system = false;
@@ -78,7 +81,6 @@ public class RelatoriosService {
 	}
 	
 	private void buscarFuncionarioDataContratacao(Scanner scanner) {
-		
 		System.out.println("Qual data de contratação?");
 		String data = scanner.next();
 		LocalDate localDate = LocalDate.parse(data, formatter);
@@ -86,6 +88,12 @@ public class RelatoriosService {
 		List<Funcionario> list = funcionarioRepository.findDataContratacaoMaior(localDate);
 		list.forEach(System.out::println);
 		
+	}
+	
+	private void pesquisaFuncionarioSalario() {
+		List<FuncionarioProjecao> list = funcionarioRepository.findFuncionarioSalario();
+		list.forEach(f -> System.out.println("Funcionário: id: " + f.getId()
+		+ " | nome: " + f.getNome() + " | salario: " + f.getSalario()));
 	}
 	
 }
